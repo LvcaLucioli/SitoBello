ce44749a7daca3814e22083b3e86a1117c5b8eec
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-08 20:32:12 +0100

    bootstrap link in the index page, bp's navbar

--- inc/header.php
+++ inc/header.php
@@ -1,27 +1,35 @@
-<nav><!-- menù superiore -->
-    <div class="conteiner-fluid">
-        <div>
-            <ul>
-                <li><a href="index.php" class="active">Home</a></li>
-                <li><a href="convenzioni.php">Convenzioni</a></li>
-                <li><a href="settori.php">Settori</a></li>
-                <li><a href="news.php">News</a></li>
-                <li><a href="documenti.php">Documenti online</a></li>
-                <li><a href="contatti.php">Contatti</a></li>
-            </ul>
-            
+<nav class="navbar navbar-expand-lg navbar-light bg-linght "><!-- menù superiore -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="convenzioni.php">Convenzioni</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="settori.php">Settori</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="documenti.php">Documenti online</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contatti.php">Contatti</a>
+            </li>
             <?php
             if (isset($_SESSION['user'])){
-                echo "<ul class='accessi nav navbar-nav navbar-right' style='margin-top: -53px; margin-right: 0;'>
-                        <li><a href='admin.php'>Ciao $_SESSION[user]</a></li>
-                        <li><a href='logout.php'>Esci</a></li>
-                     </ul>";
+                echo "<li class='nav-item'>
+                    <a class='nav-link' href='admin.php'>Ciao $_SESSION[user]</a>
+                    </li>
+                    <li class='nav-item'>
+                    <a class='nav-link' href='logout.php'>Esci</a>
+                    </li>
+                    ";
             }else{
-                echo "<ul class='accessi nav navbar-nav navbar-right' style='margin-top: -53px; margin-right: 0;'>
-                        <li><a href='login.php'>Area riservata</a></li>
-                     </ul>";
+                echo "<li class='nav-item'><a class='nav-link'href='login.php'>Area riservata</a></li>";
             }
             ?>
-        </div>
+        </ul>
+           
     </div>
 </nav>
