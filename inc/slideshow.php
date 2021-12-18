38c0af740bd4b4e3187873e3c003f873bee86fa7
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-18 23:11:55 +0100

    got rid of arrows, numbers in the slideshow

--- inc/slideshow.php
+++ inc/slideshow.php
@@ -2,29 +2,17 @@
     <div class="slideshow-container">
 
         <div class="mySlides">
-            <div class="numbertext">1 / 3</div>
-            <img src="img/skyline.jpg" style="width:100%">
+            <img src="img/skyline.jpg" style="width:100%" class="img-slideshow">
         </div>
 
         <div class="mySlides">
-            <div class="numbertext">2 / 3</div>
-            <img src="img/mare.jpg" style="width:100%">
+            <img src="img/mare.jpg" style="width:100%" class="img-slideshow">
         </div>
 
         <div class="mySlides">
-            <div class="numbertext">3 / 3</div>
-            <img src="img/gatti.jpg" style="width:100%">
+            <img src="img/gatti.jpg" style="width:100%" class="img-slideshow">
         </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
 
     </div>
     <br>
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
 </div>
