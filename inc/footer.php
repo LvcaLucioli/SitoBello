be64e69faa9dcbd9d64be47b8ac75b12e4d823b2
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-23 20:22:35 +0100

    added working footer

---
+++ inc/footer.php
@@ -0,0 +1,25 @@
+<div class="foo-ter">
+    <footer>
+        <div class="row">
+            <div class="col-4 header-container header-title-container gap">
+                <span class="header-title">
+                    <h4>Contatti</h4>
+                    <span>
+                        <h5>Scriveteci</h5>
+                    </span>
+                </span>
+            </div>
+            <div class="col-4 header-container">
+                <img class="logoACSI-foo" src="img\logoACSI.jpg" alt="Logo ACSI">
+            </div>
+            <div class="col-4 header-container header-title-container gap">
+                <span class="header-title">
+                    <h4>SIAMO QUI</h4>
+                    <span>
+                        <h5>Venite a trovarci</h5>
+                    </span>
+                </span>
+            </div>
+        </div>
+    </footer>
+</div>
