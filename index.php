6e10366766bd17fe334d4febf98f8f16946d4820
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-24 00:56:49 +0100

    fixing path and renamed home to index. moving to hosting server

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+$_SESSION['previousPage'] = 'index.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="shortout icon" href="favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css\stile_generale.css">
+	<link rel="stylesheet" type="text/css" href="css\stile_home.css">
+	<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="js/slideshow.js"></script>
+	<title>Noleggiopattino</title>
+</head>
+
+<body onload="showSlides()">
+
+	<?php
+	require_once __DIR__ . '/inc/header.php';
+	require_once __DIR__ . '/inc/sectionHome.php';
+	require_once __DIR__ . '/inc/slideshow.php';
+	?>
+</body>
+
+<?php
+require_once __DIR__ . '/inc/footer.php'; 
+?>
+
+</html>
