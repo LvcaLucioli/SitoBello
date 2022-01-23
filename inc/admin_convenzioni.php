658ecc178185502565ef2b7a8d2fe1944432df75
GiorgioFantilli <Giorgio.Fantilli@gmail.com>
2022-01-23 16:51:43 +0100

    Merging

--- inc/admin_convenzioni.php
+++ inc/admin_convenzioni.php
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-<form id="upload_convenzione" method="POST" action="post.php" enctype="multipart/form-data">
-    nome azienda<input type="text" name="company_name"><br>
-    città<input type="text" name="city"><br>
-    indirizzo<input type="text" name="address"><br>
-    numero di telefono<input type="text" name="phone"><br>
-    descrizione<input type="text" name="description"><br>
-    <input type="file" name="logo"><br>
-    <input type="submit">
-</form>
-=======
+
 <div class="col-4 header-container header-title-container gap">
     <form id="upload_convenzione" method="POST" action="post.php" enctype="multipart/form-data">
         nome azienda<input type="text" name="company_name"><br>
@@ -20,4 +10,3 @@
         <input type="submit">
     </form>
 </div>
->>>>>>> home-fix
