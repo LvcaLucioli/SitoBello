579f35dc9240f887d0529cea129ac186b1209cc7
GiorgioFantilli <Giorgio.Fantilli@gmail.com>
2022-01-26 00:15:23 +0100

    feat: implemented news style

--- inc/user_news.php
+++ inc/user_news.php
@@ -17,25 +17,32 @@ for ($i = 0; $i < $row; $i++) {
 }
 mysqli_close($connection);
 
-for ($i = 0; $i < $row; $i++) {
-    if ($i % 2 == 0) {
-        echo '<div class="row">';
+$f = 0;
+echo "<div class='news-gap'></div>";
+
+for ($i = $row - 1; $i >= 0; $i--) {
+    if (($i % 2 == 0) && ($i != $row - 1)) {
+        echo '</div>';
+        $f = 0;
     }
 
-    echo '<div class="col-4 header-container header-title-container gap">
-                <span class="header-title">';
-                if(isset($tmp[$i]['title']))    echo '<h4>' . $tmp[$i]['title'] . '</h4>';
-                    echo '<span>';
-                    if(isset($tmp[$i]['body'])) echo '<h5>' . $tmp[$i]['body'] . '</h5>';
-                    if(isset($tmp[$i]['image_path']))   echo '<img class="logoACSI-foo" src="https://storm-beaten-instan.000webhostapp.com/news/images/' . $tmp[$i]['image_path'] . '" alt="">';
-                    if(isset($tmp[$i]['attachment_path']))  echo '<iframe src="http://docs.google.com/gview?url=http://storm-beaten-instan.000webhostapp.com/news/attachments/' . $tmp[$i]['attachment_path'] . '&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>';
-                    echo '
-                    </span>
+    if ($i % 2 == 0) {
+        echo '<div class="row row-news">';
+        $f = 1;
+    }
+
+    echo '<div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 header-container header-title-container news-wrap">
+                <span class="header-title news-container th-lg">';
+
+                if($tmp[$i]['title'] != "")    echo "<h4>".$tmp[$i]['title']."</h4>";
+                echo '<span>';
+                if($tmp[$i]['body'] != "") echo '<p>' . $tmp[$i]['body'] . '</p>';
+                if($tmp[$i]['image_path'] != "")   echo '<img class="logoACSI-foo" src="https://storm-beaten-instan.000webhostapp.com/news/images/' . $tmp[$i]['image_path'] . '" alt="">';
+                if($tmp[$i]['attachment_path'] != "")  echo '<iframe src="http://docs.google.com/gview?url=http://storm-beaten-instan.000webhostapp.com/news/attachments/' . $tmp[$i]['attachment_path'] . '&embedded=true" frameborder="0"></iframe>';
+                echo '</span>
+
                 </span>
-            </div>
-        ';
-
-    if ($i % 2 == 0) {
-        echo '/<div>';
-    }
+          </div>';
 }
+
+if ($f) echo '</div>';
